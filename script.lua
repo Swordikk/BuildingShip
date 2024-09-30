fafbe6ea649e01bcda7d22538d3666fc2a07aa89
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-09-30 15:15:32 +0400

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,40 @@
+while true do
+local a, b, c, d, e, f, g, h, i = 0, 0, 0, 0, 0, 0, 0, 0, 0
+repeat a = a + 1
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage1.DarknessPart.CFrame
+wait(0.3)
+until a == 5
+repeat b = b + 1
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage2.DarknessPart.CFrame
+wait(0.3)
+until b == 5
+repeat c = c + 1
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage3.DarknessPart.CFrame
+wait(0.3)
+until c == 5
+repeat d = d + 1
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage4.DarknessPart.CFrame
+wait(0.3)
+until d == 5
+repeat e = e + 1
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage5.DarknessPart.CFrame
+wait(0.3)
+until e == 5
+repeat f = f + 1
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage6.DarknessPart.CFrame
+wait(0.3)
+until f == 5
+repeat g = g + 1
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage7.DarknessPart.CFrame
+wait(0.3)
+until g == 5
+repeat h = h + 1
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage8.DarknessPart.CFrame
+wait(0.3)
+until h == 5
+repeat i = i + 1
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage9.DarknessPart.CFrame
+wait(0.3)
+until i == 5
+wait(9)
+end
