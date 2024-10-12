904ba9dbe9d9a433141b2fa838672fda4a51be9c
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-10-12 22:25:01 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,96 +1,104 @@
-local Part1 = Instance.new("Part", game:GetService("Workspace"))
-Part1.Name = "Block"
-Part1.Anchored = true
-Part1.Transparency = 0
-Part1.Size = Vector3.new(2, 1, 2)
-Part1.CanCollide = true
-Part1.Position = Vector3.new(-51.565643310546875, 63.00004577636719, 1369.090087890625)
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Part2 = Instance.new("Part", game:GetService("Workspace"))
-Part2.Name = "Block"
-Part2.Anchored = true
-Part2.Transparency = 0
-Part2.Size = Vector3.new(2, 1, 2)
-Part2.CanCollide = true
-Part2.Position = Vector3.new(-51.565643310546875, 63.00004577636719, 2139.090087890625)
+local Window = OrionLib:MakeWindow({Name = "Script by Swordik | ⚡Tower Defense Simulator", HidePremium = false, IntroText = "Script by Swordik for TDS", SaveConfig = true, IntroEnabled = true, ConfigFolder = "Scripts"})
 
-local Part3= Instance.new("Part", game:GetService("Workspace"))
-Part3.Name = "Block"
-Part3.Anchored = true
-Part3.Transparency = 0
-Part3.Size = Vector3.new(2, 1, 2)
-Part3.CanCollide = true
-Part3.Position = Vector3.new(-51.565643310546875, 63.00004577636719, 2909.090087890625)
+local Humanoid = game.Players.LocalPlayer.Character.Humanoid
+local HumanoidRootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
 
-local Part4 = Instance.new("Part", game:GetService("Workspace"))
-Part4.Name = "Block"
-Part4.Anchored = true
-Part4.Transparency = 0
-Part4.Size = Vector3.new(2, 1, 2)
-Part4.CanCollide = true
-Part4.Position = Vector3.new(-51.565643310546875, 63.00004577636719, 3679.090087890625)
-
-local Part5 = Instance.new("Part", game:GetService("Workspace"))
-Part5.Name = "Block"
-Part5.Anchored = true
-Part5.Transparency = 0
-Part5.Size = Vector3.new(2, 1, 2)
-Part5.CanCollide = true
-Part5.Position = Vector3.new(-51.565643310546875, 63.00004577636719, 4449.08984375)
-
-local Part6 = Instance.new("Part", game:GetService("Workspace"))
-Part6.Name = "Block"
-Part6.Anchored = true
-Part6.Transparency = 0
-Part6.Size = Vector3.new(2, 1, 2)
-Part6.CanCollide = true
-Part6.Position = Vector3.new(-51.565643310546875, 63.00004577636719, 5219.08984375)
-
-local Part7 = Instance.new("Part", game:GetService("Workspace"))
-Part7.Name = "Block"
-Part7.Anchored = true
-Part7.Transparency = 0
-Part7.Size = Vector3.new(2, 1, 2)
-Part7.CanCollide = true
-Part7.Position = Vector3.new(-51.565643310546875, 63.00004577636719, 5989.08984375)
-
-local Part8 = Instance.new("Part", game:GetService("Workspace"))
-Part8.Name = "Block"
-Part8.Anchored = true
-Part8.Transparency = 0
-Part8.Size = Vector3.new(2, 1, 2)
-Part8.CanCollide = true
-Part8.Position = Vector3.new(-51.565643310546875, 63.00004577636719, 6759.08984375)
-
-local Part9 = Instance.new("Part", game:GetService("Workspace"))
-Part9.Name = "Block"
-Part9.Anchored = true
-Part9.Transparency = 0
-Part9.Size = Vector3.new(2, 1, 2)
-Part9.CanCollide = true
-Part9.Position = Vector3.new(-51.565643310546875, 63.00004577636719, 7529.08984375)
-
-while true do
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage1.DarknessPart.CFrame
-	wait(2)
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage2.DarknessPart.CFrame
-	wait(2)
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage3.DarknessPart.CFrame
-	wait(2)
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage4.DarknessPart.CFrame
-	wait(2)
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage5.DarknessPart.CFrame
-	wait(2)
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage6.DarknessPart.CFrame
-	wait(2)
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage7.DarknessPart.CFrame
-	wait(2)
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage8.DarknessPart.CFrame
-	wait(2)
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage9.DarknessPart.CFrame
-	wait(2)
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.NormalStages.CaveStage10.DarknessPart.CFrame
-	wait(2)
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BoatStages.OtherStager.ElectronicStage.DarknessPart.CFrame
-	wait(10)
+function WalkSpeed()
+	while _G.WalkSpeed do game:GetService("RunService").RenderStepped:wait()
+	    Humanoid.WalkSpeed = _G.WalkSpeed
+    end
 end
+
+function JumpPower()
+	while _G.JumpPower do game:GetService("RunService").RenderStepped:wait()
+	    Humanoid.JumpPower = _G.JumpPower
+    end
+end
+
+function AutoFarmCube()
+	while _G.AutoFarmCube == Value do
+		for i, v in pairs(game:GetService("Workspace"):GetChildren()) do
+			if v.Name == "Fortune Cube" or v.Name == "Luck Cube" or
+			v.Name == "Multitude of Rain" or
+			v.Name == "Fortune of Wind" or
+			v.Name == "Silent Speed of Snow Adds 2 walkspeed" or
+			v.Name == "Wealth Cube" or
+			v.Name == "Spore Blossom" or
+			v.Name == "Golden Cube" or
+			v.Name == "Patel Cube" or
+			v.Name == "Speed Cube" or
+			v.Name == "Haste Cube" or
+			v.Name == "Glitch Cube" then game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Hitbox.CFrame
+			end
+		  end
+	end
+end
+
+local Tab = Window:MakeTab({
+	Name = "AutoFarm",
+	Icon = "rbxassetid://4483362748",
+	PremiumOnly = false
+})
+
+Tab:AddToggle({
+	Name = "AutoFarm Cube",
+	Default = false,
+	Callback = function(Value)
+		_G.AutoFarmCube = Value
+		AutoFarmCube()
+	end    
+})
+
+
+local Tab = Window:MakeTab({
+	Name = "Misc",
+	Icon = "rbxassetid://4483362748",
+	PremiumOnly = false
+})
+
+Tab:AddTextbox({
+	Name = "WalkSpeed",
+	Default = "",
+	TextDisappear = false,
+	Callback = function(Value)
+		_G.WalkSpeed = Value
+		WalkSpeed()
+	end
+})
+
+Tab:AddTextbox({
+	Name = "JumpPower",
+	Default = "",
+	TextDisappear = false,
+	Callback = function(Value)
+		_G.JumpPower = Value
+		JumpPower()
+	end
+})
+
+Tab:AddToggle({
+	Name = "Anti-AFK",
+	Default = false,
+	Callback = function(Value)
+		if Value == true then
+			while not game:IsLoaded() do wait() end
+			repeat wait() until game.Players.LocalPlayer.Character
+			Players = game:GetService("Players")
+			local GC = getconnections or get_signal_cons
+			if GC then
+				for i,v in pairs(GC(Players.LocalPlayer.Idled)) do
+					if v["Disable"] then v["Disable"](v)
+					elseif v["Disconnect"] then v["Disconnect"](v)
+					end
+				end
+			else
+			Players.LocalPlayer.Idled:Connect(function()
+				VirtualUser:CaptureController()
+				VirtualUser:ClickButton2(Vector2.new())
+  				end)
+			end
+		end
+	end
+})
